65b9b470307903e197e6bdf522e24d193be09157
Teyojessam-Abam-14 <teyojessam.abam@gmail.com>
2024-03-27 19:35:00 -0400

    CONTROL_SCRIPT_PYTHON_MODULE_AWS_S3_Delete_S3_Bucket_Teejay_Abam

--- s3_boto3.py
+++ s3_boto3.py
@@ -8,3 +8,8 @@ def create_s3_bucket(s3,bucket_name):
         ObjectLockEnabledForBucket=True
     )
 
+#Delete Bucket
+def delete_s3_bucket(s3,bucket_name):
+   s3.delete_bucket(
+        Bucket=bucket_name
+    )
--- s3_main.py
+++ s3_main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import boto3,sys,json,os
-from s3_boto3 import create_s3_bucket 
+from s3_boto3 import delete_s3_bucket
 
 if __name__=="__main__": 
  #s3_function=sys.argv[1]
@@ -20,7 +20,7 @@ if __name__=="__main__":
  #Calling the S3 service using assumed role credentials
  s3=boto3.client('s3', aws_access_key_id=credentials['AccessKeyId'], aws_secret_access_key=credentials['SecretAccessKey'], aws_session_token=credentials['SessionToken'], region_name='us-east-1')
  
- #Calling create bucket function
- create_s3_bucket(s3, 'teejay-s3')
+ #Calling delete bucket function
+ delete_s3_bucket(s3, 'teejay-s3')
  
  
