b65974642296d98cd7684510586bbb99715a9cd3
Teyojessam-Abam-14 <teyojessam.abam@gmail.com>
2024-03-27 20:02:17 -0400

    CONTROL_SCRIPT_PYTHON_MODULE_AWS_S3_Empty_S3_Bucket_Teejay_Abam

--- s3_boto3.py
+++ s3_boto3.py
@@ -17,3 +17,20 @@ def delete_s3_bucket(s3,bucket_name):
 #Upload files to S3
 def upload_file_to_s3(s3, file_path, bucket_name, object_name):
     s3.upload_file(file_path, bucket_name, object_name, ExtraArgs={'ContentType': 'text/html'})
+    
+#Empty Bucket
+def empty_s3_bucket(s3,bucket_name):
+    
+    object_list=s3.list_objects_v2(Bucket=bucket_name)
+    
+    if "Contents" in object_list:
+        objects=[]
+        for obj in object_list["Contents"]:
+           objects.append({"Key": obj["Key"]})
+        
+        # Delete all objects in the bucket
+        s3.delete_objects(
+            Bucket=bucket_name,
+            Delete={"Objects": objects}
+        )
+    
--- s3_main.py
+++ s3_main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import boto3,sys,json,os
-from s3_boto3 import create_s3_bucket, upload_file_to_s3
+from s3_boto3 import create_s3_bucket, upload_file_to_s3, empty_s3_bucket
 
 if __name__=="__main__": 
  #s3_function=sys.argv[1]
@@ -25,10 +25,10 @@ if __name__=="__main__":
  file_path='./s3_file_for_upload.txt'
  object_name='S3 file'
  
- #Calling create bucket function
- create_s3_bucket(s3, bucket_name)
+ #Emptying bucket
+ empty_s3_bucket(s3,bucket_name)
  
- #Uploading files to created bucket
- upload_file_to_s3(s3, file_path, bucket_name, object_name)
+ #Calling create bucket function
+ 
  
  
